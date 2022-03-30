8e2b646807ab94a8a7885c67df2794f485c7baa2
ULY-SS3S <terryl@usc.edu>
2022-03-29 18:33:11 -0700

    Updated plot and load_data

--- core/timeseries.py
+++ core/timeseries.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 class TimeSeries:
     def __init__(self,data=None,link=None,nan=None):
@@ -9,6 +10,7 @@ class TimeSeries:
         elif link is not None:
             tmp_data = pd.read_csv(link)
             self.load_data(tmp_data)
+        self.indicators = None
 
     def __str__(self):
         pass
@@ -16,7 +18,7 @@ class TimeSeries:
     def __dict__(self):
         pass
 
-    def load_data(self,data):
+    def load_data(self,data,delim=',',indicator=False):
         """Loads the input data into the timeseries object
 
         :param data: dict, array-like, pd.Series
@@ -47,7 +49,17 @@ class TimeSeries:
         # If the index of data is string, change the format to datetime
         if isinstance(data, str):
             # Check if data is a filename
-            if data[-4:] == ".csv" or data[-4:] == ".txt":
+            if data[-4:] == ".csv":
+                df  = pd.from_csv(str,sep = delim)
+                df_index = df.index
+                df.data = pd.Series(data = df.iloc[0,:].tolist(),index=df_index)
+                iteritems = df.iteritems()
+                next(iteritems)
+                for (column_name,column_data) in df.iteritems():
+                    self.indicators.append(pd.Series(data=column_data,index=df_index,name=column_name))
+            if data[-4:] == ".txt":
+                pass
+
 
 
             # Else, data is a string representing the time series
@@ -72,13 +84,19 @@ class TimeSeries:
         if not all(data.index[i] <= data.index[i+1] for i in range(len(data.index) - 1)):
             raise "Time series are not in chronological order!"
 
+        if not isinstance(self.data.index,datetime.date):
+            raise "Index of the series must be datetime!"
 
 
-
-    def plot(self):
-        plt.figure()
-        if self.data is not None:
+    def plot(self,figsize=(6,8),xlabel=None,ylabel=None,title=None,plot_indicator=False,style=None):
+        plt.figure(figsize=figsize)
+        if not plot_indicator:
             plt.plot(self.data)
+        else:
+            if style='together':
+                plt.subplot(1,2,1)
+
+
 
     def to_string(self):
         return self.__str__(self)
