0fde0e25e2fc013244058e920083a4d937301fbe
ULY-SS3S <terryl@usc.edu>
2022-04-24 20:56:00 -0700

    Update Setup

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ setup(
     author_email='terryl@usc.edu',
     description='A Quantitative Finance Library',
     install_requires=[
-       'setuptools~=56.0.0',
-       'pandas~=1.4.0',
-        'numpy~=1.22.2',
-        'yfinance=0.1.70',
-        'statsmodels=0.13.2',
-        'scipy~=1.8.0',
-        'matplotlib~=3.5.1',
-        'plotly~=5.7.0',
-        'python-dateutil~=2.8.2',
-        'yahoo_fin=0.8.9.1'
+       'setuptools',
+       'pandas',
+        'numpy',
+        'yfinance',
+        'statsmodels',
+        'scipy',
+        'matplotlib',
+        'plotly',
+        'python-dateutil',
+        'yahoo_fin'
     ]
 )
