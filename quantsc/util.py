16f937fd06a13b92b2b26a6f17df1d2306464977
ULY-SS3S <terryl@usc.edu>
2022-04-25 16:23:59 -0700

    Update plotly import rules

--- quantsc/util.py
+++ quantsc/util.py
@@ -3,3 +3,8 @@ def change_plot_backend(backend):
     if backend not in qsc.config.config['supported_plot_backend']:
         raise("Plotting backend must be in " + str(qsc.config.config['supported_plot_backend']))
     qsc.config.config['plot_backend'] = backend
+
+def update_plotly_param():
+    if 'get_ipython().__class__.__name__' == 'ZMQInteractiveShell':
+        import plotly.offline as pyo
+        pyo.init_notebook_mode()
