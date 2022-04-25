29fb75dacdb52e8805c83c387a086e0cfb3ab7d5
ULY-SS3S <terryl@usc.edu>
2022-04-24 19:48:31 -0700

    Add Dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
-setup(
-    name="QSC",
-    version="1.0",
-    packages=find_packages()
-)
+
+setup(name='quantsc',
+      version='1.0',
+      description='Financial Library for Python',
+      author='Yuanhao Lu,Joanathan Qin,Aditya Prasad',
+      author_email='terryl@usc.edu',
+      url='https://www.quantsc.org',
+      packages=find_packages(),
+     )
